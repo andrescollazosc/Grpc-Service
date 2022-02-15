8c6e957347a69e70804eaacc9d69a564f00ccf43
Andres Collazos <andrescollazosc@gmail.com>
2022-02-14 21:55:59 -0500

    Added new project gRPC example in c#

---
+++ GrpcServiceACCAMD/Data/PersonRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace GrpcServiceACCAMD.Data
+{
+    public class PersonRepository
+    {
+
+        private static List<PersonResponse> _personResponses = new List<PersonResponse>();
+
+        public static async Task<PersonResponse> GetById(int id) {
+            return GetAll().FirstOrDefault(x => x.Id == id);
+        }
+
+        private static List<PersonResponse> GetAll()
+        {
+            return _personResponses = new List<PersonResponse>()
+            {
+                new PersonResponse() { Id = 1, FirstName = "Andres", LastName = "Collazos Certuche" },
+                new PersonResponse() { Id = 2, FirstName = "Alejandro", LastName = "Martinez Delgado" },
+                new PersonResponse() { Id = 3, FirstName = "Edgar", LastName = "Arciniegas" },
+            };
+        }
+
+
+
+    }
+
+    
+}
---
+++ GrpcServiceACCAMD/Services/PersonService.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+using GrpcServiceACCAMD.Data;
+
+namespace GrpcServiceACCAMD.Services
+{
+    public class PersonService : GrpcPersonService.GrpcPersonServiceBase
+    {
+        public override async Task<PersonResponse> GetPerson(StatusPersonRequest request, ServerCallContext context)
+        {
+            return await PersonRepository.GetById(request.Id);
+        }
+
+        public override Task<ResultRequest> Create(PersonRequest request, ServerCallContext context)
+        {
+            return base.Create(request, context);
+        } 
+        
+        //http://localhost:5134
+
+
+    }
+}
